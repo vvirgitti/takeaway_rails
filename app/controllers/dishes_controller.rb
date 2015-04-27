50cf41b91b8e479f16244fd689f2760245a952ee
Vanessa <virgitti@gmail.com>
2015-04-27 14:22:42 +0100

    can create a new dish feature

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,4 +4,17 @@ class DishesController < ApplicationController
     @dishes = Dish.all
   end
 
+  def create
+    @dish = Dish.create(dish_params)
+    redirect_to '/dishes'
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def dish_params
+    params.require(:dish).permit(:name, :price)
+  end
+
 end
--- spec/features/dish_features_spec.rb
+++ spec/features/dish_features_spec.rb
@@ -4,14 +4,27 @@ feature 'dishes' do
 
   context 'no dishes added yet' do
     scenario 'should display a prompt to add a dish' do
-      visit '/'
+      visit '/dishes'
       expect(page).to have_content 'No dish added yet'
       expect(page).to have_button 'Add a dish'
     end
   end
 
-  context "dishes have been added" do
 
+  context "creating a new dish" do
+    scenario "should display a promt to create a new dish, then display that dish" do
+      visit '/dishes'
+      click_on 'Add a dish'
+      fill_in 'Name', with: 'Shrimp Tempura'
+      fill_in 'Price', with: 10
+      click_button 'Create Dish'
+      expect(page).to have_content("Shrimp Tempura")
+      expect(page).to have_content 10
+      expect(current_path).to eq '/dishes'
+    end
+  end
+
+  context "dishes have been added" do
     before do
       Dish.create(name: 'Shrimp Tempura', price:10)
     end
